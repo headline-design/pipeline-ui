e4f799e5f3e0b1b7e08614324b13d3ac87feff52
headline-design <85418370+headline-design@users.noreply.github.com>
2021-06-24 17:38:23 -0500

    removed myalgo connectivity

--- src/Button/index.js
+++ src/Button/index.js
@@ -2,7 +2,6 @@ import BaseButton from './BaseButton';
 import TextButton from './TextButton';
 import OutlineButton from './OutlineButton';
 import SolidButton from './SolidButton';
-import MyAlgo from '@randlabs/myalgo-connect';
 import { LocalConvenienceStore } from '@pipeline-ui/icons/es/md';
 var Button; // solid style
 
@@ -18,19 +17,5 @@ Button.Text.displayName = 'Button.Text'; // base style
 Button.Base = BaseButton;
 Button.Base.displayName = 'Button.Base';
 
-const myAlgoWallet = new MyAlgo();
 
-const connectToMyAlgo = async() => {
-  const myAlgoWallet = new MyAlgo();
-  try {
-      const accounts = await myAlgoWallet.connect();
-  }
-  catch (err) {
-      alert(err);
-  }
-}
-
-
-
-export {myAlgoWallet, connectToMyAlgo};
 export default Button;
