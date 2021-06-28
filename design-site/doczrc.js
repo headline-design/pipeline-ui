9058f5e310d345e9398a916b11a17d51685ba95b
headline-design <aaron@pipeline-ui.com>
2021-06-28 05:49:17 -0500

    Commitx2

--- design-site/doczrc.js
+++
@@ -1,9 +0,0 @@
-const projectRoot = __dirname.replace('/.docz', '');
-
-const { menuItems } = require(`${projectRoot}/config/menu.json`);
-
-export default {
-  files: 'docs/**/*.{md,markdown,mdx}',
-  title: 'Docz & Netlify CMS',
-  menu: menuItems
-}
