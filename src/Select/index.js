cc204bf7b247597e2fc51fc5f172254ddae2b0d7
headline-design <85418370+headline-design@users.noreply.github.com>
2021-07-02 11:12:19 -0500

    Fixed Select component
    Fixed Select component, added files to gitignore

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,62 +1,8 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import Box from '../Box'
-import { ExpandMore } from '@pipeline-ui/icons/es/md'
-import { StyledInput } from '../Input'
+import Select from 'react-select'
 
-const StyledWrapper = styled(Box)`
-  & {
-    position: relative;
-    display: inline-block;
-  }
-
-  > svg {
-    pointer-events: none;
-    position: absolute;
-    right: 0;
-    top: 0;
-    bottom: 0;
-  }
-`
-
-const Select = React.forwardRef(
-  ({ options, children, width, ...props }, ref) => {
-    const renderChildren = () => {
-      if (children) {
-        return children
-      } else if (options) {
-        return options.map((option, i) => (
-          <option key={i} value={option.value} children={option.label} />
-        ))
-      } else {
-        return null
-      }
-    }
-
-    return (
-      <StyledWrapper width={width}>
-        <StyledInput
-          as={'select'}
-          children={renderChildren()}
-          ref={ref}
-          width={'100%'}
-          p={'auto'}
-          py={0}
-          pl={3}
-          pr={'3rem'}
-          {...props}
-        />
-        <ExpandMore my={'auto'} mx={3} />
-      </StyledWrapper>
-    )
-  }
-)
-
-Select.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.object),
+const CustomSelect = ({ options, onChange }) => {
+  return <Select options={options} onChange={onChange} />
 }
 
-Select.displayName = 'Select'
-
-export default Select
+export default CustomSelect
