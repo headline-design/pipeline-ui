a159a07423346ccbd8d49840d11c55f3c174404c
headline-design <aaron@pipeline-ui.com>
2021-07-11 09:23:42 +0000

    Style Upgrade

--- design-site/docusaurus.config.js
+++ design-site/docusaurus.config.js
@@ -3,6 +3,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
+    // ...
+      plugins: ['@docusaurus/theme-live-codeblock'],
+      themeConfig: {
+        liveCodeBlock: {
+          /**
+           * The position of the live playground, above or under the editor
+           * Possible values: "top" | "bottom"
+           */
+          playgroundPosition: 'bottom',
+        },
+      },
+// ...
   title: 'Pipeline UI',
   tagline: 'Integrating Algorand and React',
   url: 'https://your-docusaurus-test-site.com',
@@ -13,97 +25,105 @@ module.exports = {
   organizationName: 'headline-design', // Usually your GitHub org/user name.
   projectName: 'pipeline-ui', // Usually your repo name.
   themeConfig: {
-    navbar: {
-      title: 'PIPELINE-UI',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'getting-started-B',
-          position: 'left',
-          label: 'Getting Started',
+        announcementBar: {
+          id: 'announcementBar-1', // Increment on change
+          content:
+            '⭐️ If you like PIPELINE-UI, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/headline-design/pipeline-ui">GitHub</a>! ⭐',
+          backgroundColor: '#fafbfc', // Defaults to `#fff`.
+          textColor: '#091E42', // Defaults to `#000`.
+          isCloseable: false, // Defaults to `true`.
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+      navbar: {
+        title: 'PIPELINE-UI',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'getting-started-B',
+            position: 'left',
+            label: 'Getting Started',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/headline-design',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/docs/getting-started-B',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/headline_crypto',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/headline-design',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} HEADLINE INC. Built with PIPELINE-UI.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    },
+    presets: [
+      [
+        '@docusaurus/preset-classic',
         {
-          href: 'https://github.com/headline-design',
-          label: 'GitHub',
-          position: 'right',
+          docs: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            // Please change this to your repo.
+            editUrl:
+              'https://github.com/headline-design/pipeline-ui',
+          },
+          blog: {
+            showReadingTime: true,
+            // Please change this to your repo.
+            editUrl:
+              'https://headline.dev/',
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
         },
       ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started',
-              to: '/docs/getting-started-B',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/headline_crypto',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/headline-design',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} HEADLINE INC. Built with PIPELINE-UI.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/headline-design/pipeline-ui',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://headline.dev/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
     ],
-  ],
-};
+  };
