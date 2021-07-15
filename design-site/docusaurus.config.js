33535eaeb7b72521543fcd8eb73c846f9d6d21cb
headline-design <85418370+headline-design@users.noreply.github.com>
2021-07-15 12:45:39 -0500

    Edit GitHub patch.

--- design-site/docusaurus.config.js
+++ design-site/docusaurus.config.js
@@ -112,7 +112,7 @@ module.exports = {
             sidebarPath: require.resolve('./sidebars.js'),
             // Please change this to your repo.
             editUrl:
-              'https://github.com/headline-design/pipeline-ui',
+              'https://github.com/headline-design/pipeline-ui/blob/main/design-site',
           },
           blog: {
             showReadingTime: true,
