cf8534a9876bdea3e88c263577ad75168edb325f
Headline-Design <aaron@pipeline-ui.com>
2021-07-31 17:22:24 -0500

    demonstrate changing border radius of avatar with class "pipeline-avatar"

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import './index.css'
+import './custom.css'
 
 import {
   Avatar,
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+//import './index.css'
 import App from './App';
-import styles from './custom.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
