061e6e92c8304a3e77fc957f9e3284c57fbbadd5
headline-design <aaron@pipeline-ui.com>
2021-07-31 20:39:54 -0500

    Fuego -x4

---
+++ demo/build/precache-manifest.f29d4eeafdd067e827200ab0d8c666dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6daa7f4812815d37cc49e7c2726c1be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dee59b57b7c154e4f98",
+    "url": "/static/css/main.6946b758.chunk.css"
+  },
+  {
+    "revision": "2710d27111eef718bdd3",
+    "url": "/static/js/2.23c88b83.chunk.js"
+  },
+  {
+    "revision": "9dee59b57b7c154e4f98",
+    "url": "/static/js/main.58c19fee.chunk.js"
+  },
+  {
+    "revision": "d6c8b0338a16e1e82302",
+    "url": "/static/js/runtime-main.60d0dddc.js"
+  }
+]);
--- demo/build/service-worker.js
+++ demo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2dea011e08cd21f23bf2b87efc361426.js"
+  "/precache-manifest.f29d4eeafdd067e827200ab0d8c666dc.js"
 );
 
 self.addEventListener('message', (event) => {
