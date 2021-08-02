3587159bd0b77f5458851e421044951eaa6345a4
Headline-Design <aaron@pipeline-ui.com>
2021-08-02 10:53:22 -0500

    Fixed Blockie Component, wrote code to troubleshoot AlgoAddress copy

--- src/Blockie/index.js
+++ src/Blockie/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import blockies from 'ethereum-blockies';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import blockies from 'ethereum-blockies'
 
 class BlockiesIdenticon extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   getOpts() {
     return {
@@ -14,18 +14,25 @@ class BlockiesIdenticon extends Component {
       size: this.props.opts.size || 15,
       scale: this.props.opts.scale || 3,
       spotcolor: this.props.opts.spotcolor || '#000',
-    };
+    }
   }
   componentDidMount() {
-    this.draw();
+    this.draw()
   }
+
+  componentDidUpdate(prevProps, _prevState) {
+    if (prevProps !== this.props) {
+      this.draw()
+    }
+  }
+
   draw() {
-    blockies.render(this.getOpts(), this.canvas);
+    blockies.render(this.getOpts(), this.canvas)
   }
   render() {
     return React.createElement('canvas', {
       ref: canvas => (this.canvas = canvas),
-    });
+    })
   }
 }
 
@@ -38,7 +45,7 @@ BlockiesIdenticon.defaultProps = {
     scale: 3,
     spotcolor: '#000',
   },
-};
+}
 
 BlockiesIdenticon.propTypes = {
   /**
@@ -52,8 +59,8 @@ BlockiesIdenticon.propTypes = {
     scale: PropTypes.number,
     spotcolor: PropTypes.string,
   }),
-};
+}
 
-BlockiesIdenticon.displayName = 'BlockiesIdenticon';
+BlockiesIdenticon.displayName = 'BlockiesIdenticon'
 
-export default BlockiesIdenticon;
+export default BlockiesIdenticon
